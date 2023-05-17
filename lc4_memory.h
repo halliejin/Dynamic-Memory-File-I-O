6e8e3f9d87e48b36d64c4af2121ed5ec14c44fa7
Zihan (Hallie) Jin <117152219+halliejin@users.noreply.github.com>
2023-05-17 12:51:34 +0800

    Update lc4_memory.h

--- lc4_memory.h
+++ lc4_memory.h
@@ -2,8 +2,6 @@
 /* File Name : lc4_memory.h		 										*/
 /* Purpose   : This program contains the structure for the LC4 memory's	*/
 /* 			   linked list and the declarations of the helper functions */
-/*             															*/
-/* Author(s) : tjf 														*/
 /************************************************************************/
 
 #ifndef LC4_MEMORY_H
